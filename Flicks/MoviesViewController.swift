51007282053867402c33297e78a34e9a2178028f
Xinxin Xie <pro_cindy@hotmail.com>
2016-01-25 21:52:26 -0800

    add DetailVieeController

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -149,14 +149,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = movies[indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie
+        // Get the new view contr]oller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
